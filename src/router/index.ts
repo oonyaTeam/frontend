d770f4401460870c51f62facb2afba353ed4e096
Shoma <sho1228.k@gmail.com>
2021-03-15 00:46:20 +0900

    ルーティングの記述

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue'
+import AllWord from '../views/AllWord.vue'
+import Setting from '../views/Setting.vue'
+import Signup from '../views/Signup.vue'
+import Login from '../views/Login.vue'
+import Logout from '../views/Logout.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +16,31 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/allword',
+    name: 'AllWord',
+    component: AllWord
+  },
+  {
+    path: '/setting',
+    name: 'Setting',
+    component: Setting
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/logout',
+    name: 'Logout',
+    component: Logout
   }
 ]
 
