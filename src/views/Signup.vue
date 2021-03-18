8e795ff4f645bd2713e8b7e92e96894beb5c027a
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-03-18 21:17:15 +0900

    Update: Signup, Login

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,12 +1,68 @@
 <template>
-  <p>Signup works</p>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-item>
+        <ion-input
+          name="mail"
+          type="text"
+          placeholder="Name"
+          required
+          v-model="state.mail"
+        ></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-input
+          name="password"
+          type="text"
+          placeholder="PassWord"
+          required
+          v-model="state.passWord"
+        ></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-button
+          type="submit"
+          expand="block"
+          @click="onSubmit()"
+        >
+          Login
+        </ion-button>
+      </ion-item>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
+import { IonContent, IonPage, IonItem, IonInput, IonButton } from '@ionic/vue';
+import axios from 'axios';
+import { useStore } from 'vuex';
 
 export default defineComponent({
   name: "Signup",
+  components: {
+    IonContent,
+    IonPage,
+    IonItem,
+    IonInput,
+    IonButton,
+  },
+  setup() {
+    const state = reactive({
+      mail : "",
+      passWord: "",
+    });
+    const store = useStore();
+
+    const onSubmit = async () => {
+      console.log(state.mail + state.passWord);
+    };
+
+    return {
+      state,
+      onSubmit,
+    }
+  }
 });
 </script>
 
